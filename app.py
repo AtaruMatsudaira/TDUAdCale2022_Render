8aa00ea1392f2a4776e46b2ffdb0f49992af202d
AtaruMatsudaira <matsu222212@gmail.com>
2022-12-21 02:44:29 +0900

    fix:hostの指定を修正

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def neko():
     return "<h1>にゃーん</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
